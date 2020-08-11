9370aa9e02daa0a7748b7ecb7f8fbf4e8c3eea62
gaohonglei1219 <54Ghl1219>
2020-08-11 17:10:55 +0800

    upload mobile/web html files

--- src/main/java/com/devep/controller/WebController.java
+++ src/main/java/com/devep/controller/WebController.java
@@ -5,19 +5,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
-@CrossOrigin
 
+@RestController
+@CrossOrigin
 public class WebController {
 
-    @RequestMapping("/index")
-    public ModelAndView hello(HttpSession session){
-        ModelAndView mv = new ModelAndView("index.html");
+//    @RequestMapping("/index")
+//    public ModelAndView hello(HttpSession session){
+//        ModelAndView mv = new ModelAndView("index.html");
+//        return mv;
+//    }
+//
+//    @RequestMapping("/indexM")
+//    public ModelAndView hello1(HttpSession session){
+//        ModelAndView mv = new ModelAndView("indexM.html");
+//        return mv;
+//    }
+
+    @RequestMapping("/{htmlName}")
+    public ModelAndView hello(@PathVariable String htmlName, HttpSession session){
+
+
+        ModelAndView mv = new ModelAndView(htmlName+".html");
         return mv;
     }
-
 }
